3d8fb0fefbb503475416ea1c92066661e35d32a3
Marius Frohnhofen <marius@248studios.com>
2026-01-09 01:50:47 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,84 @@
-import Image from "next/image";
+"use client"
+
+import Link from "next/link";
+import { useState } from "react";
+
+interface ColorPalette {
+  colors: string[]
+}
+
+const colorPalettes: ColorPalette[] = [
+  {
+    colors: ["#1a1a1a", "#ef715d"]
+  },
+  {
+    colors: ["#BABFF2", "#7B84D1", "#444AAB", "#1E2D76"]
+  },
+  {
+    colors: ["#EBBB4C", "#7E8CC8", "#3C4390", "#E07B3E"]
+  },
+  {
+    colors: ["#F7AF69", "#F79D64", "#F4805D", "#F26A5A", "#F26454"]
+  },
+  {
+    colors: ["#4E795E", "#3F644D", "#2E4734", "#1D2617"]
+  }
+]
 
 export default function Home() {
+  const [currentColor, setCurrentColor] = useState<ColorPalette>(colorPalettes[Math.floor(Math.random() * colorPalettes.length)])
+
+  const [canvasWidth, setCanvasWidth] = useState<number>(320)
+  const [canvasHeight, setCanvasHeight] = useState<number>(451)
+
+  const [rows, setRows] = useState<number>(8)
+  const [columns, setColumns] = useState<number>(6)
+  
+  const [paddingX, setPaddingX] = useState<number>(45)
+  const [paddingY, setPaddingY] = useState<number>(65)
+
+  const [circleScale, setCircleScale] = useState<number>(0.3132 / columns)
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex h-[95vh] flex-col gap-8 md:gap-8 items-center justify-center roboto text-sm font-medium" style={{ backgroundColor: "#f8f6f1" }}>
+      <p style={{ width: "320px" }}>Hello! I'm Marius.<br />Developer & Founder based in Germany.</p>
+      <div className="shadow-2xl">
+        <svg
+          width={canvasWidth}
+          height={canvasHeight}
+          viewBox={`0 0 ${canvasWidth} ${canvasHeight}`}
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ display: "block" }}
+        >
+          <rect
+            x="0"
+            y="0"
+            width={canvasWidth}
+            height={canvasHeight}
+            fill="#F1EAD8"
+          />
+          {Array.from({ length: rows }).flatMap((_, row) =>
+            Array.from({ length: columns }).map((_, col) => (
+              <circle
+                key={`${row}-${col}`}
+                cx={col * ((canvasWidth - (2 * paddingX)) / (columns - 1)) + paddingX}
+                cy={row * ((canvasHeight - (2 * paddingY)) / (rows - 1)) + paddingY}
+                r={canvasWidth * circleScale}
+                fill={
+                  currentColor.colors[
+                  Math.floor(Math.random() * currentColor.colors.length)
+                  ]
+                }
+              />
+            ))
+          )}
+        </svg>
+      </div>
+      <div className="flex flex-row gap-8 items-center">
+        <Link href="https://github.com/248studios">GITHUB</Link>
+        <Link href="https://248studios.com">248 STUDIOS</Link>
+        <Link href="/about">ABOUT</Link>
+      </div>
     </div>
   );
 }
